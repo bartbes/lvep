2a0de3e03c8f7e611952eea7dcf82d54b04dea89
Bart van Strien <60183+bartbes@users.noreply.github.com>
2016-05-06 18:07:38 +0200

    Add LVEPDecoder

--- test/main.lua
+++ test/main.lua
@@ -1,13 +1,18 @@
 function love.load()
 	package.cpath = "./lib?.so"
-	newStream = require "lvep"
-	f = love.filesystem.newFile("sintel.mp4")
-	str = newStream(f)
-	vid = love.graphics.newVideo(str)
-	--audio = love.audio.newSource("sintel.ogv", "stream")
-	--str:setSync(audio)
-	--audio:play()
-	vid:play()
+	newVideoStream, newDecoder = unpack(require "lvep")
+	do
+		local f = love.filesystem.newFile("sintel.mp4")
+		vs = newVideoStream(f)
+	end
+	do
+		local f = love.filesystem.newFile("sintel.mp4")
+		d = newDecoder(f)
+	end
+	vid = love.graphics.newVideo(vs)
+	aud = love.audio.newSource(d)
+	vs:setSync(aud)
+	aud:play()
 end
 
 function love.draw()
